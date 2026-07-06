98342024978947bd1cffe3f86a6d87fe8c22ec87
kotshidtanihso <kotshidtanihso@gmail.com>
2026-07-07 02:11:13 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- index.php
+++ index.php
@@ -23,6 +23,19 @@
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a4c12cc539b7e1d4b7d4d6f/1jssiisnu';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </head>
 
 <body>
